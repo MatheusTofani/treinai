e5eadc971ea3fc3c20419962c6d7dcfe26176bc5
Matheus <matheus271112@gmail.com>
2024-10-17 07:35:46 -0300

    Iniciando projeto

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import styled from 'styled-components'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View >
+      <Teste>Ola <Logo>TreinAi</Logo></Teste>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Teste = styled.Text `
+
+font-size: 50px;
+`
+
+const Logo = styled.Text `
+font-weight: bold;
+color: #E16517;
+`
