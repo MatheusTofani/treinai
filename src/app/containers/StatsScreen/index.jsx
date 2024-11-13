c16bd7d005ef13c6de871a51b4b869b600d33dd5
pedrotofani <pedrotofani@wiz.co>
2024-11-13 18:08:47 -0300

    feat: Adicionar subida Modal e Serie

--- src/app/containers/StatsScreen/index.jsx
+++ src/app/containers/StatsScreen/index.jsx
@@ -1,15 +1,20 @@
 import { useFocusEffect } from '@react-navigation/native';
 import React, { useCallback, useEffect, useState } from 'react';
-import { View, Text, FlatList } from 'react-native';
+import { View, Text, FlatList, SafeAreaView, TouchableOpacity } from 'react-native';
 import { getFirestore, collection, getDocs, doc, getDoc } from 'firebase/firestore';
 import { firestore } from '../../../firebaseConfig'; 
 import { useUser } from '../../contexts/UserContext'; 
-import { Container, Card, Nome, Peso, Data, TreinoText } from './style';
+import { Container, Card, Nome, Peso, Data, TreinoText, ModalContainer } from './style';
 
 const StatsScreen = () => {
   const { userId } = useUser(); // Pega o userId do contexto
   const [userData, setUserData] = useState(null);
   const [treinos, setTreinos] = useState([]);
+  const [activeCardId, setActiveCardId] = useState(null);
+
+  const handleCardClick = (id) => {
+    setActiveCardId((prev) => (prev === id ? null : id)); // Alterna entre abrir e fechar
+  };
 
   const fetchUserData = useCallback(async () => {
     if (userId) {
@@ -27,13 +32,18 @@ const StatsScreen = () => {
         const treinosRef = collection(firestore, 'users', userId, 'treinos');
         const treinosSnapshot = await getDocs(treinosRef);
         const treinosData = treinosSnapshot.docs
-          .map((doc) => ({
-            id: doc.id,
-            ...doc.data(),
-          }))
-          .sort((a, b) => b.createdAt.toDate() - a.createdAt.toDate()); // Ordena por data, do mais novo para o mais antigo
+        .map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }))
+        .sort((a, b) => b.createdAt.toDate() - a.createdAt.toDate()); // Ordena por data, do mais novo para o mais antigo
 
-        setTreinos(treinosData);
+   
+  
+        // Ordena os treinos por data de criação (mais recente primeiro)
+        const treinosOrdenados = treinosData.sort((a, b) => b.createdAt.toDate() - a.createdAt.toDate());
+  
+        setTreinos(treinosOrdenados);
       } catch (error) {
         console.log('Erro ao buscar dados:', error.message);
       }
@@ -50,30 +60,54 @@ const StatsScreen = () => {
     // Lógica para formatação de data e qualquer outro processamento
     const createdAtDate = item.createdAt ? item.createdAt.toDate() : null;
     const formattedDate = createdAtDate ? createdAtDate.toLocaleDateString('pt-BR') : 'N/A';
+    const isActive = activeCardId === item.id;
   
     // Retorna o JSX para cada treino
     return (
-      <Card>
-        <Nome>{item.nome || 'Sem nome'}</Nome>
-        <Peso>Peso: {item.peso || 'N/A'} kg</Peso>
-        <Data>Criado Em: {formattedDate}</Data>
-    </Card>
+      <View>
+        <TouchableOpacity onPress={() => handleCardClick(item.id)}>
+          <Card>
+            <Nome>{item.nome || 'Sem nome'}</Nome>
+            <Peso>Peso: {item.peso || 'N/A'} kg</Peso>
+            <Data>Criado Em: {formattedDate}</Data>
+          </Card>
+        </TouchableOpacity>  
+          {isActive && (
+            <ModalContainer>
+              <Text>Séries: </Text>
+              {item.series && item.series.length > 0 ? (
+                item.series.map((serie) => (
+                  <View key={serie.id}>
+                    <Text>Tempo de Descanso: {serie.tempoDescanso || 'N/A'} Seg</Text>
+                    <Text>Repetições: {serie.repeticoes || 'N/A'}</Text>
+                    <Text>Peso: {serie.peso || 'N/A'} kg</Text>
+                    <Text>Nota: {serie.nota || 'N/A'}</Text>
+                  </View>
+                ))
+              ) : (
+                <Text>Sem séries registradas para este treino.</Text>
+              )}
+            </ModalContainer>
+          )}
+      </View>
     );
   };
   
 
   return (
-    <Container>
-      {treinos.length > 0 ? (
-        <FlatList
-          data={treinos}
-          keyExtractor={(item) => item.id}
-          renderItem={renderTreino}
-        />
-      ) : (
-        <TreinoText>Carregando treinos...</TreinoText>
-      )}
-    </Container>
+    <SafeAreaView style={{ flex: 1 }}>
+      <Container>
+        {treinos.length > 0 ? (
+          <FlatList
+            data={treinos}
+            keyExtractor={(item) => item.id}
+            renderItem={renderTreino}
+          />
+        ) : (
+          <TreinoText>Nenhum treino encontrado!</TreinoText>
+        )}
+      </Container>
+    </SafeAreaView>
   );
   
 };
